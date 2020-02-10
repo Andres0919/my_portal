bf706d585730c9f2ef5edbef472e6675959d9208
andresposada09 <andres.posada0919@gmail.com>
2020-02-10 08:46:38 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="bg-white min-h-screen">
+    <Header />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
